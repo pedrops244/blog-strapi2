b1cb37e034387f526a2fb0c0777b0c3acb77a1a1
Pedro Souza <pedrops244@hotmail.com>
2023-12-16 17:03:27 -0300

    more edits for strapi app

--- src/api/post/services/post.js
+++ src/api/post/services/post.js
@@ -1,9 +1,9 @@
 'use strict';
 
 /**
- * product service
+ * post service
  */
 
 const { createCoreService } = require('@strapi/strapi').factories;
 
-module.exports = createCoreService('api::product.product');
+module.exports = createCoreService('api::post.post');
