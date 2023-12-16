2a56e90f2bf90f638c3742e8af65511a25a25d95
Pedro Souza <pedrops244@hotmail.com>
2023-12-16 13:57:10 -0300

    configs

--- config/database.js
+++ config/database.js
@@ -1,42 +1,13 @@
-const path = require("path");
-
-module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT");
-
-  const connections = {
-    postgres: {
-      connection: {
-        connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST"),
-        port: env.int("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        user: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
-        ssl: env.bool("DATABASE_SSL") && {
-          key: env("DATABASE_SSL_KEY"),
-          cert: env("DATABASE_SSL_CERT"),
-          ca: env("DATABASE_SSL_CA"),
-          capath: env("DATABASE_SSL_CAPATH"),
-          cipher: env("DATABASE_SSL_CIPHER"),
-          rejectUnauthorized: env.bool(
-            "DATABASE_SSL_REJECT_UNAUTHORIZED",
-            true
-          ),
-        },
-        schema: env("DATABASE_SCHEMA", "public"),
-      },
-      pool: {
-        min: env.int("DATABASE_POOL_MIN", 2),
-        max: env.int("DATABASE_POOL_MAX", 10),
-      },
-    },
-  };
-
-  return {
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
     connection: {
-      client,
-      ...connections[client],
-      acquireConnectionTimeout: env.int("DATABASE_CONNECTION_TIMEOUT", 60000),
+      host: env("DATABASE_HOST"),
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      ssl: env.bool("DATABASE_SSL", true),
     },
-  };
-};
+  },
+});
