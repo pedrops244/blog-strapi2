64a85a37abbd33ff88b7ee18974f0c323ce56e8e
Pedro Souza <pedrops244@hotmail.com>
2023-12-24 17:21:46 -0300

    more configs

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -753,6 +753,7 @@ export interface ApiPostPost extends Schema.CollectionType {
     singularName: 'post';
     pluralName: 'posts';
     displayName: 'post';
+    description: '';
   };
   options: {
     draftAndPublish: true;
@@ -760,7 +761,6 @@ export interface ApiPostPost extends Schema.CollectionType {
   attributes: {
     title: Attribute.String & Attribute.Required;
     cover: Attribute.Media & Attribute.Required;
-    content: Attribute.Blocks & Attribute.Required;
     slug: Attribute.UID<'api::post.post', 'title'> & Attribute.Required;
     author: Attribute.Relation<
       'api::post.post',
@@ -772,6 +772,7 @@ export interface ApiPostPost extends Schema.CollectionType {
       'manyToOne',
       'api::category.category'
     >;
+    content: Attribute.RichText & Attribute.Required;
     createdAt: Attribute.DateTime;
     updatedAt: Attribute.DateTime;
     publishedAt: Attribute.DateTime;
