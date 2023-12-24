d220d0bbbf8ce4380fd6c2d37ff5d1e98e82dd08
Pedro Souza <pedrops244@hotmail.com>
2023-12-24 17:10:55 -0300

    configs

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -1,15 +1,14 @@
 "use strict";
 
 /**
- * post controller
+ *  product controller
  */
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-(module.exports = createCoreController("api::post.post")),
-  {
-    count(ctx) {
-      var { query } = ctx.request;
-      return strapi.query("api::post.post").count({ where: query });
-    },
-  };
+module.exports = createCoreController("api::post.post", {
+  count(ctx) {
+    var { query } = ctx.request;
+    return strapi.entityService.count("api::post.post", query);
+  },
+});
