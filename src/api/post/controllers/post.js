328a434987ce9152d77510e070f0f429fbedce60
Pedro Souza <pedrops244@hotmail.com>
2023-12-30 18:43:34 -0300

    remove lifecycles

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -8,19 +8,6 @@ const axios = require("axios");
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::post.post", {
-  lifecycles: {
-    async afterCreate(result, data) {
-      axios.post(
-        "https://api.vercel.com/v1/integrations/deploy/prj_kKGyjYCxsZ4jUbhkDMEWZUTYhlGZ/YphG1viGPJ"
-      );
-    },
-
-    async afterUpdate(result, params, data) {
-      axios.post(
-        "https://api.vercel.com/v1/integrations/deploy/prj_kKGyjYCxsZ4jUbhkDMEWZUTYhlGZ/YphG1viGPJ"
-      );
-    },
-  },
   count(ctx) {
     var { query } = ctx.request;
     return strapi.entityService.count("api::post.post", query);
